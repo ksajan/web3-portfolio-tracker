8a3ec1968cdf16e5c85f2856569196bb443da1c8
ksajan <ksajan12@gmail.com>
2024-07-06 19:33:26 +0530

    feat: make docker build runable
    fixed python depency problem
    fixed typescript npx not found issue
    improve the dockerfile

--- deployment/app.sh
+++ deployment/app.sh
@@ -9,4 +9,14 @@ export LOG_LEVEL=${LOG_LEVEL:-warning}
 current_timestamp="$(date "+%Y-%m-%d %H:%M:%S")"
 echo "Running script at $current_timestamp"
 
+echo "Loading Node and Installing npm packages"
+
+# Load nvm and install Node.js
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install npm packages
+npm install
+
+echo "Running Crypto-Quants Portfolio Tracker"
 exec gunicorn main:app --workers $WORKERS --worker-class uvicorn.workers.UvicornWorker --bind $HOST:$PORT --timeout $TIMEOUT --log-level $LOG_LEVEL
